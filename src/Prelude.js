f46b7afabaa32725e6d5e75855c505f23a54c070
zonxin <0zonxin0@163.com>
2016-04-22 21:17:57 +0800

    add guard 函数
    add curryingN

--- src/Prelude.js
+++ src/Prelude.js
@@ -1,19 +1,18 @@
 define([
     './core.js',
+    './Void.js',
     './Ordering/EQ.js'
-],function(jHaskell,EQ){
+],function(jHaskell,Void,EQ){
 
     var Prelude = {
         elem: function(a,list){
-            var len = list.length,
-                i;
+            var len = list.length, i;
             for(i=0;i<len;i++){
-                if( list[i].equal(a)){
-                    return true;
-                }
+                if( list[i].equal(a)){ return true; }
             }
             return false;
-        }
+        },
+        guard:function(b){ return b? [Void]:[]; }
     };
 
     return Prelude;
