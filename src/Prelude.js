d26549f6e63316994d5d73fc11deed4d04214070
zonxin <0zonxin0@163.com>
2016-05-10 11:28:48 +0800

    添加 maybe either 函数

--- src/Prelude.js
+++ src/Prelude.js
@@ -1,8 +1,14 @@
 define([
     './core.js',
     './Unit.js',
-    './Ordering/EQ.js'
-],function(jHaskell,Unit,EQ){
+    './Ordering/EQ.js',
+    './Maybe/Maybe.js',
+    './Maybe/Nothing.js',
+    './Maybe/Just.js',
+    './Either/Either.js',
+    './Either/Left.js',
+    './Either/Right.js'
+],function(jHaskell,Unit,EQ,Maybe,Nothing,Just,Either,Left,Right){
 
     var Prelude = {
         elem: function(a,list){
@@ -12,7 +18,19 @@ define([
             }
             return false;
         },
-        guard:function(b){ return b? [Unit]:[]; }
+        guard:function(b){ return b? [Unit]:[]; },
+        // maybe :: b -> (a -> b) -> Maybe a -> b
+        maybe: function(d,fn,ma,r){
+            if(ma === Nothing) { return d; }
+            return fn(ma.value);
+        },
+        // either :: (a -> c) -> (b -> c) -> Either a b -> c
+        either: function(fa,fb,eab,c){
+            var itis = eab.isLeft();
+            if(itis){ return fa(itis.value); }
+            itis = eab.isRight();
+            return fb(itis.value);
+        }
     };
 
     return Prelude;
